a1a5c6b9e1cc5ab0c2809a7bdb5f9d30204635c7
陈林林 <chen724207709a3@163.com>
2016-03-25 16:09:52 +0800

    dfd

--- ceshi/ceshi/ViewController.m
+++ ceshi/ceshi/ViewController.m
@@ -16,7 +16,14 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"dsfadsfdsfdsfsd");    NSLog(@"dsfadsfdsfdsfsd");
+    NSLog(@"dsfadsfdsfdsfsd");
+    NSLog(@"dsfadsfdsfdsfsd");
+    NSLog(@"dsfadsfdsfdsfsd");
+    NSLog(@"dsfadsfdsfdsfsd");
+    NSLog(@"dsfadsfdsfdsfsd");
+
+
 }
 
 - (void)didReceiveMemoryWarning {

d4fedd7e70b195a07dec4c02cc80a7151a6120f8
陈林林 <chen724207709a3@163.com>
2016-03-25 16:08:14 +0800

    diiic

---
+++ ceshi/ceshi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ceshi
+//
+//  Created by lanou3g on 16/3/25.
+//  Copyright © 2016年 陈林林. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ceshi/ceshi/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ceshi
+//
+//  Created by lanou3g on 16/3/25.
+//  Copyright © 2016年 陈林林. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
